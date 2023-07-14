401e8a630c4921f822c0b7d6263e65a241adff1d
Anuj Dwivedi <anujd0009@gmail.com>
2023-07-14 19:41:48 +0530

    working-story-select-v.02

--- app.py
+++ app.py
@@ -22,7 +22,41 @@ stories = [
         "tone": "neutral",
         "paragraph": "Once, there was an enchanted castle on top of a hill. It was said to hold many secrets and treasures. Many adventurers tried to uncover its mysteries, but only a few succeeded. The castle had hidden rooms, secret passages, and magical artifacts."
     },
-    # Add more stories here
+    {
+        "title": "The Brave Knight",
+        "tone": "positive",
+        "paragraph": "In the kingdom of Elysia, there was a brave knight named Sir Arthur. He was known for his unwavering courage and chivalry. Sir Arthur embarked on daring quests to protect the realm from evil forces and restore peace and justice."
+    },
+    {
+        "title": "The Mysterious Island",
+        "tone": "neutral",
+        "paragraph": "Deep in the heart of the vast ocean, there lay a mysterious island shrouded in mist. Legends spoke of hidden treasures and mythical creatures dwelling on its shores. Many adventurers set sail in search of this enigmatic island, but few returned with tales to tell."
+    },
+    {
+        "title": "The Secret Garden",
+        "tone": "positive",
+        "paragraph": "Behind the old mansion, there was a secret garden untouched by time. Its lush greenery and vibrant flowers created a serene oasis. The garden held the power to heal and bring joy to those who discovered its hidden beauty."
+    },
+    {
+        "title": "The Lost City",
+        "tone": "neutral",
+        "paragraph": "Buried deep within the dense jungle, there existed a lost city filled with ancient ruins and untold stories. The city held the remnants of a once-great civilization, waiting to be unraveled by curious explorers."
+    },
+    {
+        "title": "The Haunted Manor",
+        "tone": "negative",
+        "paragraph": "On a desolate hill, stood a haunted manor with a dark past. Its halls echoed with ghostly whispers, and shadows danced along its decaying walls. Many believed the manor was cursed, and dared not venture near after sunset."
+    },
+    {
+        "title": "The Time Traveler's Journal",
+        "tone": "positive",
+        "paragraph": "Within the pages of an ancient journal, a time traveler chronicled their extraordinary adventures across different eras. From the majestic courts of Renaissance Europe to the futuristic landscapes of the 31st century, the journal revealed the wonders and perils of time travel."
+    },
+    {
+        "title": "The Legendary Sword",
+        "tone": "neutral",
+        "paragraph": "Legends spoke of a mythical sword with the power to vanquish any foe. It was said to be hidden deep within a treacherous mountain range, guarded by fierce creatures and ancient traps. Many warriors set out on a quest to wield this legendary weapon and bring peace to the land."
+    }
 ]
 
 @app.route('/')
